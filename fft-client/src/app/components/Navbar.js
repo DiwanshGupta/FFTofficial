5632a6897b4dd31f3488862bae4282adddec8968
DiwanshGupta <dewanshgupta120@gmail.com>
2024-03-09 22:36:41 +0530

    first commit

---
+++ fft-client/src/app/components/Navbar.js
@@ -0,0 +1,82 @@
+"use client";
+import React, { useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { HiMenu } from "react-icons/hi";
+import { RxCross2 } from "react-icons/rx";
+import { FaHome } from "react-icons/fa";
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <>
+      <section>
+        <div className="bg-white/80 shadow-md fixed top-0 left-0 w-full z-40 ease-in duration-300 backdrop-blur-md">
+          <div className="py-3 px-2 md:px-16 lg:px-16 items-center container mx-auto ">
+            <div className="flex  items-center  justify-around">
+              <div className="flex flex-row items-center gap-2 ">
+                <div className="block md:hidden z-40" onClick={handleNav}>
+                  {nav ? (
+                    <>
+                      <RxCross2
+                        size={20}
+                        className="text-[#bbc84a] cursor-pointer"
+                      />
+                    </>
+                  ) : (
+                    <>
+                      {" "}
+                      <HiMenu
+                        size={20}
+                        className="text-yellow-700 cursor-pointer"
+                      />
+                    </>
+                  )}
+                </div>
+                <img
+                  src="fft.jpg"
+                  className="md:h-[4rem] h-[3rem] w-[3rem] rounded-full md:w-[4rem]"
+                  alt="FFT"
+                />{" "}
+              </div>
+              <div className="flex hidden md:block justify-start items-center gap-8">
+                <div className="flex gap-4 font-semibold text-2xl text-yellow-600">
+                  <span className=" flex items-center gap-1">
+                    <FaHome /> Home
+                  </span>
+                  <span>Content</span>
+                </div>
+              </div>
+              <div className="flex flex-row items-center gap-5 md:gap-11">
+                <span className="font-semibold text-yellow-500 text-lg animate-bounce   flex items-center gap-1 ">
+                  <AiOutlineShoppingCart />
+                  Store
+                </span>
+                <button className="md:text-lg  text-white bg-yellow-600 rounded-lg p-3 font-semibold text-xs">
+                  Login &#47; Register
+                </button>
+              </div>
+            </div>
+            <div
+              className={`absolute w-1/2 md:hidden sm:w-2/5 h-screen py-2 px-3 text-xl font-medium ease-in shadow-sm backdrop-blur-md bg-white/90 top-0 duration-200 ${
+                nav ? "left-0" : "left-[-100%]"
+              } pt-20`}
+            >
+              <div className="flex flex-col gap-8 items-center font-semibold text-xl text-yellow-500">
+                <span className="border-b border-yellow-500/35 w-24  justify-center flex">
+                  Home
+                </span>
+                <span className="border-b border-yellow-500/35 w-24  justify-center flex">
+                  Content
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ fft-client/src/app/components/testimoials.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <section>
+      <div className="container mx-auto py-[14vh]">Testimonials</div>
+    </section>
+  );
+};
+
+export default Testimonials;
